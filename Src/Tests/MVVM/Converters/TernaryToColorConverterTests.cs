3d806cc0e4e0996a8368c0cef507cc5f39624310
Coding Enthusiast <CodingEnthusiast@protonmail.com>
2021-09-20 17:39:24 +0430

    Add some tests

---
+++ Src/Tests/MVVM/Converters/TernaryToColorConverterTests.cs
@@ -0,0 +1,32 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2021 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using Avalonia.Media;
+using HandyDandy.Models;
+using HandyDandy.MVVM.Converters;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests.MVVM.Converters
+{
+    public class TernaryToColorConverterTests
+    {
+        public static IEnumerable<object[]> GetCases()
+        {
+            yield return new object[] { TernaryState.Unset, Brushes.Transparent };
+            yield return new object[] { TernaryState.One, Brushes.LightGreen };
+            yield return new object[] { TernaryState.Zero, Brushes.LightGreen };
+        }
+        [Theory]
+        [MemberData(nameof(GetCases))]
+        public void ConvertTest(TernaryState value, ISolidColorBrush expected)
+        {
+            var conv = new TernaryToColorConverter();
+            object actual = conv.Convert(value, null, null, null);
+            Assert.IsAssignableFrom<ISolidColorBrush>(actual);
+            Assert.Equal(expected, (ISolidColorBrush)actual);
+        }
+    }
+}
---
+++ Src/Tests/MVVM/Converters/TernaryToStringConverterTests.cs
@@ -0,0 +1,26 @@
+﻿// Autarkysoft Tests
+// Copyright (c) 2021 Autarkysoft
+// Distributed under the MIT software license, see the accompanying
+// file LICENCE or http://www.opensource.org/licenses/mit-license.php.
+
+using HandyDandy.Models;
+using HandyDandy.MVVM.Converters;
+using Xunit;
+
+namespace Tests.MVVM.Converters
+{
+    public class TernaryToStringConverterTests
+    {
+        [Theory]
+        [InlineData(TernaryState.Unset, "?")]
+        [InlineData(TernaryState.One, "1")]
+        [InlineData(TernaryState.Zero, "0")]
+        public void ConvertTest(TernaryState value, string expected)
+        {
+            var conv = new TernaryToStringConverter();
+            object actual = conv.Convert(value, null, null, null);
+            Assert.IsType<string>(actual);
+            Assert.Equal(expected, (string)actual);
+        }
+    }
+}
